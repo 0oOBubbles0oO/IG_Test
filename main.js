44bcb3cb2f7a8d41ae796226dd94451d3f5cd7f9
0oOBubbles0oO <141888002+0oOBubbles0oO@users.noreply.github.com>
2023-08-10 19:17:52 -0400

    Added number formatting

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
+var saveGame = localStorage.getItem('goldMinerSave')
 var gameData = {
   gold: 0,
   goldPerClick: 1,
-  goldPerClickCost: 10
+  goldPerClickCost: 10,
+  lastTick: Date.now()
+}
+
+function update(id, content) {
+  document.getElementById(id).innerHTML = content;
 }
 
 function mineGold() {
   gameData.gold += gameData.goldPerClick
-  document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+  update("goldMined", format(gameData.gold, "scientific") + " Gold Mined")
 }
 
 function buyGoldPerClick() {
@@ -14,20 +20,33 @@ function buyGoldPerClick() {
     gameData.gold -= gameData.goldPerClickCost
     gameData.goldPerClick += 1
     gameData.goldPerClickCost *= 2
-    document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
-    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
+    update("goldMined", format(gameData.gold, "scientific") + " Gold Mined")
+    update("perClickUpgrade", "Upgrade Pickaxe (Currently Level " + format(gameData.goldPerClick, "scientific") + ") Cost: " + format(gameData.goldPerClickCost, "scientific") + " Gold")
   }
 }
 
 var mainGameLoop = window.setInterval(function() {
-  mineGold()
+  diff = Date.now() - gameData.lastTick;
+  gameData.lastTick = Date.now()
+  gameData.gold += gameData.goldPerClick * (diff / 1000)
+  update("goldMined", format(gameData.gold, "scientific") + " Gold Mined")
+  update("perClickUpgrade", "Upgrade Pickaxe (Currently Level " + format(gameData.goldPerClick, "scientific") + ") Cost: " + format(gameData.goldPerClickCost, "scientific") + " Gold")
 }, 1000)
-
-var savegame = JSON.parse(localStorage.getItem("goldMinerSave"))
-if (savegame !== null) {
-  gameData = savegame
-}
 
 var saveGameLoop = window.setInterval(function() {
   localStorage.setItem('goldMinerSave', JSON.stringify(gameData))
 }, 15000)
+
+function format(number, type) {
+	let exponent = Math.floor(Math.log10(number))
+	let mantissa = number / Math.pow(10, exponent)
+	if (exponent < 3) return number.toFixed(1)
+	if (type == "scientific") return mantissa.toFixed(2) + "e" + exponent
+	if (type == "engineering") return (Math.pow(10, exponent % 3) * mantissa).toFixed(2) + "e" + (Math.floor(exponent / 3) * 3)
+}
+
+
+if (typeof saveGame.gold !== "undefined") gameData.gold = saveGame.gold;
+if (typeof saveGame.goldPerClick !== "undefined") gameData.goldPerClick = saveGame.goldPerClick;
+if (typeof saveGame.goldPerClickCost !== "undefined") gameData.goldPerClickCost = saveGame.goldPerClickCost;
+if (typeof saveGame.lastTick !== "undefined") gameData.lastTick = saveGame.lastTick;
